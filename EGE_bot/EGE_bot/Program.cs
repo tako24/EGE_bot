7be18404ccdff004c906303482c10a1bb7aeab04
tako24 <frenk6795@gmail.com>
2021-10-10 13:57:29 +0500

    FirstStep

---
+++ EGE_bot/EGE_bot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace inf_sdamgia_bot
+{
+
+    class Program
+    {
+        private static string Token { get; } = "2061132160:AAHRnhICjh2hP3RlwXUopKKDZ1aBx3ZRfMg";
+        private static TelegramBotClient client;
+        static void Main(string[] args)
+        {
+            client = new TelegramBotClient(Token);
+            client.StartReceiving();
+            client.OnMessage += OnMessageHandler;
+            Console.ReadLine();
+            client.StopReceiving();
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void OnMessageHandler(object sender, MessageEventArgs e)
+        {
+            var message = e.Message;
+            if (message != null)
+                Console.WriteLine("Check");
+
+        }
+    }
+}
