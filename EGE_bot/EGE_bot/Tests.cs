4e478f479c18df6446ced2105a8200daa343b7fd
Авагян Арман <69074875+Aravaman@users.noreply.github.com>
2021-10-23 22:44:50 +0500

    Mimi test
    Add test TaskChekAnswer0

--- EGE_bot/EGE_bot/Tests.cs
+++ EGE_bot/EGE_bot/Tests.cs
@@ -1,62 +1,45 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
 
-//namespace EGE_bot
-//{
-//    [TestFixture]
-//    class TaskTests
-//    {
-//        [Test]
-//        public void TaskConstruct()
-//        {
-//            var str0 = "q|a|t|pp".Split('|');
-//            var task0 = new Task(str0);
-//            Assert.AreEqual("q", task0.Question);
-//            Assert.AreEqual("a", task0.Answer);
-//            Assert.AreEqual("pp", task0.PicturePath);
-//            Assert.AreEqual("t", task0.Theme);
-//            var str1 = "q|a|pp|t".Split('|');
-//            var task1 = new Task("q","a","t","pp");
-//            Assert.AreEqual("q", task1.Question);
-//            Assert.AreEqual("a", task1.Answer);
-//            Assert.AreEqual("pp", task1.PicturePath);
-//            Assert.AreEqual("t", task1.Theme);
-//        }
-//        [Test]
-//        public void TaskConstructWithEmptyPP()
-//        {
-//            var str0 = "q|a|t|".Split('|');
-//            var task0 = new Task(str0);
-//            Assert.AreEqual("q", task0.Question);
-//            Assert.AreEqual("a", task0.Answer);
-//            Assert.AreEqual("", task0.PicturePath);
-//            Assert.AreEqual("t", task0.Theme);
-//            var str1 = "q|a|t".Split('|');
-//            var task1 = new Task(str0);
-//            Assert.AreEqual("q", task1.Question);
-//            Assert.AreEqual("a", task1.Answer);
-//            Assert.AreEqual("", task1.PicturePath);
-//            Assert.AreEqual("t", task1.Theme);
-//        }
-//    }
-//    [TestFixture]
-//    class QuestionsTests
-//    {
-//        static string[] str0 = "question0|answer0|theme0|picturePath0".Split('|');
-//        Task task0 = new Task("question0", "answer0", "theme0", "picturePath0");
-//        Task task1 = new Task("question1", "answer1", "theme1", "picturePath1");
-//        Task task2 = new Task(str0);
-//        Data questions = new Data(File.ReadAllText(@"D:\C# tests\EGE_bot\EGE_bot\EGE_bot\bin\Debug\Questions\CorrectJsonFormat.json"));
-//        [Test]
-//        public void IndexerGetter_Fails_WhenIndexIsWrong()
-//        {
-//            Assert.Catch<ArgumentException>(() => { var a = questions[15]; });
-//        }
-//    }
+namespace EGE_bot
+{
+    [TestFixture]
+    class TaskTests
+    {
+        [Test]
+        public void TaskConstruct()
+        {
+            var task0 = Data.AllQuestions;
+            //Assert.AreEqual("По каналу связи передаются сообщения, содержащие только семь букв: А, Б, Г, И, М, Р, Я. Для передачи используется двоичный код, удовлетворяющий условию Фано. Кодовые слова для некоторых букв известны: А — 010, Б — 00, Г — 101. Какое наименьшее количество двоичных знаков потребуется для кодирования слова МАГИЯ? Примечание.Условие Фано означает, что ни одно кодовое слово не является началом другого кодового слова.", task0.Question);
+            Assert.AreEqual("14", task0[0].Answer); ;
+        }
 
-//}
+        [Test]
+        public void TaskChekAnswer0()
+        {
+            var task0 = new Task();
+            task0.Answer = "15";
+            Assert.AreEqual(true, task0.Check("15"));
+        }
+    }
+
+    //[TestFixture]
+    //class QuestionsTests
+    //{
+    //    static string[] str0 = "question0|answer0|theme0|picturePath0".Split('|');
+    //    Task task0 = new Task("question0", "answer0", "theme0", "picturePath0");
+    //    Task task1 = new Task("question1", "answer1", "theme1", "picturePath1");
+    //    Task task2 = new Task(str0);
+    //    Data questions = new Data(File.ReadAllText(@"D:\C# tests\EGE_bot\EGE_bot\EGE_bot\bin\Debug\Questions\CorrectJsonFormat.json"));
+    //    [Test]
+    //    public void IndexerGetter_Fails_WhenIndexIsWrong()
+    //    {
+    //        Assert.Catch<ArgumentException>(() => { var a = questions[15]; });
+    //    }
+    //}
+}
