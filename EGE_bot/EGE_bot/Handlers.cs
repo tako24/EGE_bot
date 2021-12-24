f6d158fe5821f3226aab2e5fdf83b98854d2646a
vittifavier <59151100+vittifavier@users.noreply.github.com>
2021-12-24 14:20:28 +0500

    Удаление пользователя при выборе задания

--- EGE_bot/EGE_bot/Handlers.cs
+++ EGE_bot/EGE_bot/Handlers.cs
@@ -116,7 +116,7 @@ namespace EGE_bot
             {
                 if (user.ChatId == message.Chat.Id)
                 {
-                    if (message.Text == @"Полный вариант") 
+                    if (message.Text == @"Полный вариант" || message.Text == @"Выбор задания") 
                     {
                         Program.users.Remove(user);
                         break;
