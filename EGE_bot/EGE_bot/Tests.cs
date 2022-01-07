566b2a0410dc55323091bed04c5c3619b8426965
Авагян Арман <69074875+Aravaman@users.noreply.github.com>
2022-01-07 22:32:54 +0500

    Add test

--- EGE_bot/EGE_bot/Tests.cs
+++ EGE_bot/EGE_bot/Tests.cs
@@ -1,62 +1,130 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 
-//namespace EGE_bot
-//{
-//    [TestFixture]
-//    class TaskTests
-//    {
-//        [Test]
-//        public void TaskConstruct()
-//        {
-//            var str0 = "q|a|t|pp".Split('|');
-//            var task0 = new Task(str0);
-//            Assert.AreEqual("q", task0.Question);
-//            Assert.AreEqual("a", task0.Answer);
-//            Assert.AreEqual("pp", task0.PicturePath);
-//            Assert.AreEqual("t", task0.Theme);
-//            var str1 = "q|a|pp|t".Split('|');
-//            var task1 = new Task("q","a","t","pp");
-//            Assert.AreEqual("q", task1.Question);
-//            Assert.AreEqual("a", task1.Answer);
-//            Assert.AreEqual("pp", task1.PicturePath);
-//            Assert.AreEqual("t", task1.Theme);
-//        }
-//        [Test]
-//        public void TaskConstructWithEmptyPP()
-//        {
-//            var str0 = "q|a|t|".Split('|');
-//            var task0 = new Task(str0);
-//            Assert.AreEqual("q", task0.Question);
-//            Assert.AreEqual("a", task0.Answer);
-//            Assert.AreEqual("", task0.PicturePath);
-//            Assert.AreEqual("t", task0.Theme);
-//            var str1 = "q|a|t".Split('|');
-//            var task1 = new Task(str0);
-//            Assert.AreEqual("q", task1.Question);
-//            Assert.AreEqual("a", task1.Answer);
-//            Assert.AreEqual("", task1.PicturePath);
-//            Assert.AreEqual("t", task1.Theme);
-//        }
-//    }
-//    [TestFixture]
-//    class QuestionsTests
-//    {
-//        static string[] str0 = "question0|answer0|theme0|picturePath0".Split('|');
-//        Task task0 = new Task("question0", "answer0", "theme0", "picturePath0");
-//        Task task1 = new Task("question1", "answer1", "theme1", "picturePath1");
-//        Task task2 = new Task(str0);
-//        Data questions = new Data(File.ReadAllText(@"D:\C# tests\EGE_bot\EGE_bot\EGE_bot\bin\Debug\Questions\CorrectJsonFormat.json"));
-//        [Test]
-//        public void IndexerGetter_Fails_WhenIndexIsWrong()
-//        {
-//            Assert.Catch<ArgumentException>(() => { var a = questions[15]; });
-//        }
-//    }
+namespace EGE_bot
+{
+    [TestFixture]
+    class TaskTest
+    {
+        [Test]
+        public void SnatchOutOfJson()
+        {
+            var task0 = AllTasks.Tasks[0];
+            Assert.AreEqual("46", task0.Answer); ;
+        }
 
-//}
+        [Test]
+        public void TaskChekAnswerOne()
+        {
+            var task0 = AllTasks.Tasks[0];
+            Assert.AreEqual(true, task0.Check("46"));
+        }
+
+        [Test]
+        public void TaskChekAnswerTwo()
+        {
+            var task0 = AllTasks.Tasks[0];
+            Assert.AreEqual(false, task0.Check("15"));
+        }
+
+        [Test]
+        public void IndexerGetter_Fails_WhenIndexIsWrongOne()
+        {
+            List<Task> task1 = AllTasks.Tasks;
+            string[] str0 = "question0|answer0|theme0|picturePath0".Split('|');
+            task1[4].Question = str0[0];
+            task1[4].Answer = str0[1];
+            task1[4].Theme = str0[2];
+            task1[4].PicturePath = str0[3];
+            Assert.AreEqual("theme0", task1[4].Theme);
+        }
+
+        [Test]
+        public void IndexerGetter_Fails_WhenIndexIsWrongTwo()
+        {
+            List<Task> task1 = AllTasks.Tasks;
+            task1[4].Question = "question0";
+            task1[4].Answer = "answer0";
+            task1[4].Theme = "theme0";
+            task1[4].PicturePath = "picturePath0";
+            Assert.AreEqual("theme0", task1[4].Theme);
+        }
+    }
+
+    [TestFixture]
+    class VariantTest
+    {
+        [Test]
+        public void VariantTestOne()
+        {
+            Variant variant = new Variant("Сравнение двух способов передачи данных");
+            Assert.AreEqual("Верно", variant.OnMessageSend("A216"));
+        }
+
+        [Test]
+        public void VariantTestTwo()
+        {
+            Variant variant = new Variant("лова по порядку");
+            Assert.AreEqual("Заменим буквы А, О, У на 0, 1, 2(для них порядок очевиден – по возрастанию) Выпишем начало списка, " +
+                "заменив буквы на цифры: 1. 00000 2. 00001 3. 00002 4. 00010 ... Полученная запись есть числа, " +
+                "записанные в троичной системе счисления в порядке возрастания. Тогда на 210 месте будет стоять число 209 " +
+                "(т. к. первое число 0). Переведём число 209 в троичную систему (деля и снося остаток справа налево): " +
+                "209 / 3 = 69 (2) 69 / 3 = 23 (0) 23 / 3 = 7 (2) 7 / 3 = 2 (1) 2 / 3 = 0(2) В троичной системе 209 запишется как 21202. " +
+                "Произведём обратную замену и получим УОУАУ.", variant.OnMessageSend("A216"));
+        }
+
+        [Test]
+        public void VariantTestTree()
+        {
+            Variant variant = new Variant("лова по порядку");
+            Assert.AreEqual("Все 5-буквенные слова, составленные из букв А, О, У, записаны в алфавитном порядке. " +
+                "Вот начало списка: 1. ААААА 2. ААААО 3. ААААУ 4. АААОА …… Запишите слово, которое стоит на 210-м месте от начала списка.",
+                variant.GetCurrentQuestion());
+        }
+
+        [Test]
+        public void VariantTestFour()
+        {
+            Variant variant = new Variant("Подсчёт путей с обязательной вершиной");
+            Assert.AreEqual("https://inf-ege.sdamgia.ru/get_file?id=25035", variant.GetCurrentPicturepath());
+        }
+    }
+
+    [TestFixture]
+    class HandlersTest
+    {
+        [Test]
+        public void HandlersTestOne()
+        {
+            //To be continued
+        }
+    }
+
+    [TestFixture]
+    class UserTest
+    {
+        [Test]
+        public void UserTestOne()
+        {
+            User user = new User(0, "");
+            var userVar = user.ChatId == 2;
+            Assert.AreEqual(false, userVar);
+        }
+
+        [Test]
+        public void UserTestTwo()
+        {
+            User user = new User(0, "");
+            var userVar = user.ChatId == 0;
+            Assert.AreEqual(true, userVar);
+        }
+    }
+}
